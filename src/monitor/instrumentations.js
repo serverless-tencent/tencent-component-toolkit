6bb1719c506f7663f673c388cc6bff4a77ddc738
yugasun <yuga_sun@163.com>
2020-05-06 16:24:07 +0800

    fix: remove monitor

--- src/monitor/instrumentations.js
+++
@@ -1,8 +0,0 @@
-const { MODULE_TYPE } = require('./constants')
-
-module.exports = function instrumentations() {
-  return {
-    express: { type: MODULE_TYPE.WEB_FRAMEWORK },
-    'tencent-serverless-http': { type: MODULE_TYPE.PROXY }
-  }
-}
