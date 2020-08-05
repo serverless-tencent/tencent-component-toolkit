775357b72c2ea080d22828e74910d3008ab8a365
yugasun <yuga_sun@163.com>
2020-08-05 15:58:44 +0800

    fix(cos): create bucket error message

--- src/modules/cos/index.test.js
+++ src/modules/cos/index.test.js
@@ -1,50 +1,51 @@
-const CosUtils = require('./index');
+const Client = require('./index');
 
-class ClientTest {
-	async cosTest() {
-    const APP_ID = '1251556596';
-    const bucketName = 'test-bucket';
-		const cos = new CosUtils({
-			SecretId: '',
-			SecretKey: '',
-		});
-		const cosDemo = {
-			bucket: `${bucketName}-${APP_ID}`,
-			force: true,
-			acl: {
-				permissions: 'private',
-			},
-			tags: [
-				{
-					key: 'test',
-					value: 'abcd',
-				},
-			],
-			rules: [
-				{
-					status: 'Enabled',
-					id: 'deleteObject',
-					filter: '',
-					expiration: { days: '10' },
-					abortIncompleteMultipartUpload: { daysAfterInitiation: '10' },
-				},
-			],
-		};
-		const result = await cos.deploy(cosDemo);
-		console.log(JSON.stringify(result));
-		console.log(result);
+async function runTest() {
+  const APP_ID = '1251556596';
+  const bucketName = 'test-bucket';
+  const cos = new Client({
+    SecretId: '',
+    SecretKey: '',
+  });
+  const inputs = {
+    bucket: `${bucketName}-${APP_ID}`,
+    force: true,
+    acl: {
+      permissions: 'private',
+    },
+    tags: [
+      {
+        key: 'test',
+        value: 'abcd',
+      },
+    ],
+    rules: [
+      {
+        status: 'Enabled',
+        id: 'deleteObject',
+        filter: '',
+        expiration: { days: '10' },
+        abortIncompleteMultipartUpload: { daysAfterInitiation: '10' },
+      },
+    ],
+  };
+  const result = await cos.deploy(inputs);
+  console.log(result);
 
+  await cos.upload({
+    bucket: `${bucketName}-${APP_ID}`,
+    dir: '../../utils/',
+  });
 
-		await cos.upload({
-			bucket: `${bucketName}-${APP_ID}`,
-			dir: '../../utils/',
-		});
+  await cos.remove({
+    bucket: `${bucketName}-${APP_ID}`,
+  });
 
-		await cos.remove({
-			bucket: `${bucketName}-${APP_ID}`,
-		});
-
-	}
 }
 
-new ClientTest().cosTest();
+runTest();
+
+process.on('unhandledRejection', (e) => {
+  console.log(e);
+});
+
