ab3167423e5dc1dac1e4865c0eb235393747f67e
yugasun <yuga_sun@163.com>
2020-08-18 11:23:36 +0800

    feat(cfs): add cfs module

---
+++ src/modules/cfs/apis/apis.js
@@ -0,0 +1,55 @@
+const { TypeError } = require('../../../utils/error');
+
+function apiFactory(actions) {
+  const apis = {};
+  actions.forEach((action) => {
+    apis[action] = async (capi, inputs) => {
+      const data = {
+        Version: '2019-07-19',
+        Action: action,
+        ...inputs,
+      };
+      try {
+        const { Response } = await capi.request(
+          data,
+          // this is preset options for capiateway
+          {
+            debug: false,
+            ServiceType: 'cfs',
+            // baseHost: 'tencentcloudapi.com'
+            host: 'cfs.tencentcloudapi.com',
+          },
+          false,
+        );
+
+        if (Response && Response.Error && Response.Error.Code) {
+          throw new TypeError(
+            `API_CFS_${action}`,
+            Response.Error.Message,
+            null,
+            Response.RequestId,
+          );
+        }
+        return Response;
+      } catch (e) {
+        throw new TypeError(`API_CFS_${action}`, e.message, e.stack, e.reqId);
+      }
+    };
+  });
+
+  return apis;
+}
+
+const ACTIONS = [
+  'CreateCfsFileSystem',
+  'DescribeCfsFileSystems',
+  'UpdateCfsFileSystemName',
+  'UpdateCfsFileSystemPGroup',
+  'UpdateCfsFileSystemSizeLimit',
+  'DeleteCfsFileSystem',
+  'DescribeMountTargets',
+  'DeleteMountTarget',
+];
+const APIS = apiFactory(ACTIONS);
+
+module.exports = APIS;
