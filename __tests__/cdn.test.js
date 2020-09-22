dadb7fcc52cf3dcfdd9ac4e76e43be7e226617ec
yugasun <yuga_sun@163.com>
2020-09-22 15:06:38 +0800

    fix(cdn): optimize cdn deploy and use v3 sign

--- __tests__/cdn.test.js
+++ __tests__/cdn.test.js
@@ -1,15 +1,16 @@
 const { Cdn } = require('../src');
+const { getCdnByDomain } = require('../src/modules/cdn/utils');
 
 describe('Cdn', () => {
+  jest.setTimeout(600000);
   const credentials = {
     SecretId: process.env.TENCENT_SECRET_ID,
     SecretKey: process.env.TENCENT_SECRET_KEY,
   };
   const inputs = {
-    async: true,
+    async: false,
     area: 'overseas',
     domain: 'test.yuga.chat',
-    hostType: 'cos',
     origin: {
       origins: ['up6pwd9-89hm718-xxx.cos-website.ap-guangzhou.myqcloud.com'],
       originType: 'cos',
@@ -31,10 +32,9 @@ describe('Cdn', () => {
   };
   const cdn = new Cdn(credentials, process.env.REGION);
 
-  test('should deploy CDN success', async () => {
+  test('should deploy CDN success with originType = cos', async () => {
     const res = await cdn.deploy(inputs);
     expect(res).toEqual({
-      created: true,
       https: true,
       domain: inputs.domain,
       origins: inputs.origin.origins,
@@ -42,6 +42,27 @@ describe('Cdn', () => {
       inputCache: JSON.stringify(inputs),
       resourceId: expect.stringContaining('cdn-'),
     });
+  });
+
+
+  test('should deploy CDN success with originType = domain', async () => {
+    inputs.origin.originType = 'domain';
+    const res = await cdn.deploy(inputs);
+    expect(res).toEqual({
+      https: true,
+      domain: inputs.domain,
+      origins: inputs.origin.origins,
+      cname: `${inputs.domain}.cdn.dnsv1.com`,
+      inputCache: JSON.stringify(inputs),
+      resourceId: expect.stringContaining('cdn-'),
+    });
+  });
+
+  test('should remove CDN success', async () => {
+    const res = await cdn.remove(inputs);
+    expect(res).toEqual({});
+    const detail = await getCdnByDomain(cdn.capi, inputs.domain);
+    expect(detail).toBeUndefined();
   });
 });
 
