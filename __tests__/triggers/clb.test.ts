1efef47d373eafe5d26522ae39900090b9ccdd68
yugasun <yuga_sun@163.com>
2021-03-12 14:48:32 +0800

    test: fix clb trigger test

--- __tests__/triggers/clb.test.ts
+++ __tests__/triggers/clb.test.ts
@@ -1,7 +1,7 @@
-// import { sleep } from '@ygkit/request';
+import { sleep } from '@ygkit/request';
 import { CreateClbTriggerOutput } from './../../src/modules/triggers/interface/clb';
 import { ClbTriggerInputsParams } from '../../src/modules/triggers/interface';
-// import { Scf } from '../../src';
+import { Scf } from '../../src';
 import ClbTrigger from '../../src/modules/triggers/clb';
 
 describe('Clb Trigger', () => {
@@ -10,7 +10,7 @@ describe('Clb Trigger', () => {
     SecretKey: process.env.TENCENT_SECRET_KEY,
   };
   const client = new ClbTrigger({ credentials, region: process.env.REGION });
-  // const scfClient = new Scf(credentials, process.env.REGION);
+  const scfClient = new Scf(credentials, process.env.REGION);
 
   const data: ClbTriggerInputsParams = {
     qualifier: '$DEFAULT',
@@ -18,7 +18,7 @@ describe('Clb Trigger', () => {
     protocol: 'HTTP',
     domain: '81.71.86.84',
     port: 80,
-    url: '/',
+    url: '/trigger-test',
     weight: 20,
   };
 
@@ -50,42 +50,42 @@ describe('Clb Trigger', () => {
     });
   });
 
-  // test('delete clb trigger', async () => {
-  //   const { Triggers = [] } = await scfClient.request({
-  //     Action: 'ListTriggers',
-  //     Namespace: namespace,
-  //     FunctionName: functionName,
-  //     Limit: 100,
-  //   });
-  //   const [exist] = Triggers.filter((item) => {
-  //     const { ResourceId } = item;
+  test('delete clb trigger', async () => {
+    const { Triggers = [] } = await scfClient.request({
+      Action: 'ListTriggers',
+      Namespace: namespace,
+      FunctionName: functionName,
+      Limit: 100,
+    });
+    const [exist] = Triggers.filter((item) => {
+      const { ResourceId } = item;
 
-  //     if (ResourceId.indexOf(`${output.loadBalanceId}/${output.listenerId}/${output.locationId}`)) {
-  //       return true;
-  //     }
-  //     return false;
-  //   });
+      if (ResourceId.indexOf(`${output.loadBalanceId}/${output.listenerId}/${output.locationId}`)) {
+        return true;
+      }
+      return false;
+    });
 
-  //   const res = await client.delete({
-  //     scf: scfClient,
-  //     inputs: {
-  //       namespace: namespace,
-  //       functionName: functionName,
-  //       triggerDesc: exist.TriggerDesc,
-  //       triggerName: exist.TriggerName,
-  //       qualifier: exist.Qualifier,
-  //     },
-  //   });
-  //   expect(res).toEqual({ requestId: expect.any(String), success: true });
-  // });
+    const res = await client.delete({
+      scf: scfClient,
+      inputs: {
+        namespace: namespace,
+        functionName: functionName,
+        triggerDesc: exist.TriggerDesc,
+        triggerName: exist.TriggerName,
+        qualifier: exist.Qualifier,
+      },
+    });
+    expect(res).toEqual({ requestId: expect.any(String), success: true });
+  });
 
-  // test('delete clb rule', async () => {
-  //   await sleep(3000);
-  //   const res = await client.clb.deleteRule({
-  //     loadBalanceId: output.loadBalanceId,
-  //     listenerId: output.listenerId,
-  //     locationId: output.locationId,
-  //   });
-  //   expect(res).toBe(true);
-  // });
+  test('delete clb rule', async () => {
+    await sleep(3000);
+    const res = await client.clb.deleteRule({
+      loadBalanceId: output.loadBalanceId,
+      listenerId: output.listenerId,
+      locationId: output.locationId,
+    });
+    expect(res).toBe(true);
+  });
 });
